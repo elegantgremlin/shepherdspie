8e6c416941d51cb1684934edc1e69602993488df
Chris Kirk <chrisjhkirk@gmail.com>
2021-03-27 21:37:44 -0400

    Heroku setup

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const userRouter = require('./routes/Users');
 
 require('dotenv').config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(console.log('Connected!'), (err) => console.log(err));
@@ -21,6 +23,9 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use('/users', userRouter);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
